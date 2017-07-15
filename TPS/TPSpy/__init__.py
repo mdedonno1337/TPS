39165931dd947b52cfa0ce0749ae09bfb79717be
Marco De Donno <Marco.DeDonno@unil.ch>
2017-07-15 18:11:46 +0200

    Remove the dependency to MDmisc

--- TPS/TPSpy/__init__.py
+++ TPS/TPSpy/__init__.py
@@ -10,14 +10,28 @@ from scipy.spatial.distance import cdist
 
 import numpy as np
 
-from MDmisc import fuckit
-from MDmisc.xfrange import xfrange
-
 from ..config import *
 
 ################################################################################
 #    Misc functions
 ################################################################################
+
+def xfrange( start, stop = None, step = 1.0 ):
+    """
+        Generator function for float xrange()
+        
+        >>> [ x for x in xfrange( 0, 1, 0.12 ) ]
+        [0.0, 0.12, 0.24, 0.36, 0.48, 0.6, 0.72, 0.84, 0.96]
+    """
+    if stop is None:
+        stop = float( start )
+        start = 0.0
+
+    cur = float( start )
+
+    while cur <= stop:
+        yield cur
+        cur += step
 
 def unit_vector( vector ):
     v = vector / np.linalg.norm( vector )
@@ -248,8 +262,10 @@ def grid( *args, **kwargs ):
             xp = int( ( x - range[ 'minx' ] ) * float( res ) / 25.4 )
             yp = int( ( y - range[ 'miny' ] ) * float( res ) / 25.4 )
             
-            with fuckit:
+            try:
                 pixels[ xp + dm, sizey - ( yp + dm ) ] = 0
+            except:
+                pass
             
     for i in xfrange( miny, maxy, major_step ):
         for j in xfrange( minx, maxx, minor_step ):
@@ -258,8 +274,10 @@ def grid( *args, **kwargs ):
             xp = int( ( x - range[ 'minx' ] ) * float( res ) / 25.4 )
             yp = int( ( y - range[ 'miny' ] ) * float( res ) / 25.4 )
             
-            with fuckit:
+            try:
                 pixels[ xp + dm, sizey - ( yp + dm ) ] = 0
+            except:
+                pass
     
     return img
  
