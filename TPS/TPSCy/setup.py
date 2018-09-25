f6c1a84ecb4ea267af3db1ef7bbc5990903cf6a5
Marco De Donno <Marco.DeDonno@unil.ch>
2018-09-25 15:08:09 +0200

    Update Cython to the version 0.28.5, and patch the installation process

--- TPS/TPSCy/setup.py
+++ TPS/TPSCy/setup.py
@@ -14,7 +14,7 @@ setup(
            sources = [ "TPSCy.pyx" ],
            libraries = [ 'm', 'gomp', 'pthread' ],
            include_dirs = [ numpy.get_include() ],
-           extra_compile_args = [ '-O3', '-fopenmp', '-fdce', '-ffast-math', '-Wfatal-errors', '-w' ],
+           extra_compile_args = [ '-fopenmp', '-fdce', '-ffast-math', '-Wfatal-errors', '-w' ],
            extra_link_args = []
         )
     ]
